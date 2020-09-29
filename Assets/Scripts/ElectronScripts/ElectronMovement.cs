56a48d520ef4cf6cd5935a6e96c2c2833988be9a
sebastianmh <sebastian.manriquezh@gmail.com>
2020-09-28 23:51:27 -0300

    base targets

---
+++ Assets/Scripts/ElectronScripts/ElectronMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectronMovement : MonoBehaviour
+{
+    public float angleRotation = 10f;
+    Vector3[] vectorAxises = new Vector3[7];
+    public Axistorotate axisRotate;
+    public Transform parentObject;
+    void Start()
+    {   
+        vectorAxises[0] = Vector3.back;
+        vectorAxises[1] = Vector3.down;
+        vectorAxises[2] = Vector3.forward;
+        vectorAxises[3] = Vector3.left;
+        vectorAxises[4] = Vector3.right;
+        vectorAxises[5] = Vector3.up;
+        vectorAxises[6] = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.RotateAround(parentObject.position, axis: vectorAxises[(int)axisRotate], angle: angleRotation);
+        transform.RotateAround(parentObject.position, axis: vectorAxises[(int)axisRotate], angle: angleRotation);
+    }
+
+    public enum Axistorotate { Back, Down, Forward, Left, Right, Up, Zero };
+
+}
---
+++ Assets/Scripts/UI Managers/StartSceneMenuManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartSceneMenuManager : MonoBehaviour
+{   
+    [Header("Loading Screen")]
+    public GameObject loadingScreen;
+    public Slider sliderLoadingScreen;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnClickStartApp(int indexStartScene)
+    {
+        // SceneManager.LoadScene(indexStartScene);
+        StartCoroutine("LoadYourAsyncScene", indexStartScene);
+    }
+
+    IEnumerator LoadYourAsyncScene(int indexStartScene)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(indexStartScene);
+        loadingScreen.SetActive(true);
+        while (!asyncLoad.isDone)
+        {
+            float progress = Mathf.Clamp01(asyncLoad.progress / .9f);
+            sliderLoadingScreen.value = progress;
+            yield return null;
+        }
+    }
+
+    public void OnClickHowItWorks()
+    {
+        // SHOW PANEL HOW IT WORKS
+
+    }
+
+    public void OnClickExitApp()
+    {
+        Application.Quit();
+    }
+
+
+}
